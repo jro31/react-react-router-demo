48f0f855f81c78fa6eecd892c77981537d16b7cb
Jethro Williams <jethrowilliams@outlook.com>
2021-11-01 12:01:02 +0000

    Add the <Switch> component to allow only one route to be 'active' at a time, and add the 'exact' prop to require an exact URL to match a <Route> path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Welcome from './pages/welcome'; // It's not necesary (they could still be in the 'components' folder), but a good convention is to store these "route" components in a separate 'pages' folder
 import Products from './pages/Products';
@@ -10,23 +10,28 @@ function App() {
 		<div>
 			<MainHeader />
 			<main>
-				<Route path='/welcome'>
-					<Welcome />
-				</Route>
-				<Route path='/products'>
-					<Products />
-				</Route>
-				<Route path='/products/:productId'>
-					<ProductDetail />
-				</Route>
+				<Switch>
+					<Route path='/welcome'>
+						<Welcome />
+					</Route>
+					<Route path='/products' exact>
+						<Products />
+					</Route>
+					<Route path='/products/:productId'>
+						<ProductDetail />
+					</Route>
+				</Switch>
 			</main>
 		</div>
 	);
 }
 
-// In this example, 'my-domain.com/products/p1' "matches" the path for the <Products /> component and the <ProductDetail /> component
-// Because in the 'react-router' world, 'matches' means that the path starts with the path defined in the <Route> component
-// So 'my-domain.com/products/p1' will display both <Products />, and then <ProductDetail /> below it
-// In some instances you may want this, and this is a good way to achieve it
+// The <Switch> component can be wrapped around the <Route> components, at which point only one <Route> can be active at the same time
+// The 'active' <Route> will be the one that is matched first
+// So one solution, to make sure that the <ProductDetail /> component is displayed and the <Products /> component isn't, is to change the order they written in this file, so that <ProductDetail /> is first
+// However, the solution used here is to add the 'exact' prop to '<Route path='/products' exact>'.
+// In this case, the URL is only considered a 'match' to the path, if it is an exact match, rather than just matching the beginning of the path
+// So 'my-domain.com/products' will display the <Products /> component
+// And 'my-domain.com/products/p1' will display just the <ProductDetail /> component
 
 export default App;

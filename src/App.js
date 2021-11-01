e36a12597fb4e7f79bb452bd5a01b6c2020ad434
Jethro Williams <jethrowilliams@outlook.com>
2021-11-01 10:31:56 +0000

    Add a basic routing setup, to display different components for different URLs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route } from 'react-router-dom';
+
+import Welcome from './pages/welcome'; // It's not necesary (they could still be in the 'components' folder), but a good convention is to store these "route" components in a separate 'pages' folder
+import Products from './pages/Products';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<Route path='/welcome'>
+				<Welcome />
+			</Route>
+			<Route path='/products'>
+				<Products />
+			</Route>
+		</div>
+	);
 }
+
+// The <Welcome /> componeont will be displayed when the url is 'my-domain.com/welcome'
+// The <Products /> component will be displayed when the url is 'my-domain.com/products'
+// 'my-domain.com' or 'my-domain.com/anything-else' will display nothing
 
 export default App;
